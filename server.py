c6686350f81e3c2dc5db1edc93a0179ca8adf177
Samuel Christensen <samachr@gmail.com>
2016-12-02 12:20:57 -0700

    made moving average so it won't get too rigid over time

--- server.py
+++ server.py
@@ -31,7 +31,11 @@ def postresults(username):
             previous_totalcorrect = previous[5]
             previous_averagetime = previous[6]
 
-            new_average = (result['time'] + previous_averagetime * previous_totalanswered) / (previous_totalanswered + 1)
+            if previous_totalanswered <= 10:
+                new_average = (result['time'] + previous_averagetime * previous_totalanswered) / (previous_totalanswered + 1)
+            else:
+                new_average = (result['time'] + previous_averagetime * 9) / 10
+
             db.update("update userdata set totalanswered = %s, totalcorrect = %s, averagetime = %s where id = %s",
                 [previous['totalanswered'] + 1, previous['totalcorrect'] + 1 if result['correct'] else 0,  new_average, previous['id']])
 
@@ -50,7 +54,7 @@ def getExercise(username):
             all_possible[keynum, scaledegree] = float("inf")
 
     for data in userdata:
-        all_possible[int(data[2]), data[3]] = data[6] - data[5]
+        all_possible[int(data[2]), data[3]] = float(data[6]) + 10000 / data[5]
 
     next_set = []
     for data in sorted(all_possible, key=all_possible.get, reverse=True)[:5]:
